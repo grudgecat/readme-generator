5c65f95499883f775ca0c785fe226a9df0424925
grudgecat <grudgecat@protonmail.com>
2021-07-10 16:25:36 -0700

    added functions to generate markdown

--- index.js
+++ index.js
@@ -3,26 +3,93 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require("./utils/generateMarkdown");
 
+let answers = [];
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'fullName',
+    message: 'What is your full name?',
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title for your Project ReadMe file?',
+  },
+  {
+    type:'input',
+    name: 'description',
+    message: 'Please enter a description for your project.'
+  },
+  {
+    type:'input',
+    name: 'installation',
+    message: 'Enter brief installation instructions for your project.'
+  },
+  {
+    type:'input',
+    name: 'usage',
+    message: 'Enter brief usage information.'
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'Which license do you wish to use (use space to select option)?',
+    choices: ['Apache', 'Boost', 'BSD', 'GNU_GPLv2', 'GNU_GPLv3', 'MIT', 'Unlicense',],
+  },
+  {
+    type:'input',
+    name: 'contributing',
+    message: 'Enter brief instructions for how to contribute to this project.'
+  },
+  {
+    type:'input',
+    name: 'tests',
+    message: 'Enter testing information for project.'
+  },
+  {
+    type:'input',
+    name: 'githubName',
+    message: 'Enter your GitHub user name.'
+  },
+  {
+    type:'input',
+    name: 'email',
+    message: 'Enter your email address.'
+  },
+];
 
-questions = 
 inquirer  
-  .prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title for your Project ReadMe file?',
-    }
-    .then((data) => {
-        // const filename = `${data.name.toLowerCase().split(' ').join('')}`; 
-        // fs.writeFile(filename); 
+  .prompt(questions)
+  .then((data) => {
+    const fullName = `${data.fullName}`;
+    const filename = `${data.title.toLowerCase().split(' ').join('')}`;
+    const description = `${data.description}`;
+    const installation = `${data.installation}`;
+    const usage = `${data.usage}`;
+    const license = `${data.license}`;
+    const contributing = `${data.contributing}`;
+    const tests = `${data.tests}`;
+    const githubName = `${data.githubName}`;
+    const email = `${data.email}`;
 
-    })
+    answers.push(fullName, filename, description, installation, usage, license, contributing, tests, githubName, email);
+    showMe();
+    init(); 
+  })
+.catch((error) => {
+  console.log(error);
+});
+
+function showMe() {
+console.log(answers);
+console.log('first item at 0: ', answers[0]); 
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile('output.md',`$
+  fs.writeFile(fileName,`
       OUTPUT MARKDOWN GOES HERE
       `, (err) => 
       err ? console.error(err) : console.log('Success!'))
@@ -30,13 +97,9 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-
-    //USE INQUIRER PROMPT TO GET DATA
-      // THEN CREATE TEXT FOR README FILE - CALL GENERATE MARKDOWN(ANSWERS) STORE IN MARKDOWNTEXT
-      
-    //USE GENERATED MARKDOWN TEXT TO WRITE TO FILE
+  // THEN CREATE TEXT FOR README FILE - CALL GENERATE MARKDOWN(ANSWERS) STORE IN MARKDOWNTEXT
+  generateMarkdown(answers);
+  // USE GENERATED MARKDOWN TEXT - call writeToFile - TO WRITE TO FILE
 
 }
 
-// Function call to initialize app
-init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,26 +1,160 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
+  switch (license) {
+    case 'Apache':
+      return `[![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)]`;
+      break;
+    case 'Boost':
+      return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]`;
+      break;
+    case 'BSD':
+      return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-orange.svg)]`;
+      break;
+    case 'GNU_GPLv2':
+      return `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)]`;
+      break;
+    case 'GNU_GPLv3':
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`;
+      break;
+    case 'MIT':
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`;
+      break;
+    default:
+      return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]`;
+  }
 }
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
-
+  switch(license) {
+    case 'Apache':
+      return `http://www.apache.org/licenses/LICENSE-2.0`;
+      break;
+      case 'Boost':
+        return `https://www.boost.org/LICENSE_1_0.txt`;
+        break;
+      case 'BSD':
+        return `https://opensource.org/licenses/BSD-3-Clause`;
+        break;
+      case 'GNU_GPLv2':
+        return `        ${badge} ${link}`;
+        break;
+      case 'GNU_GPLv3':
+        return `        ${badge} ${link}`;
+        break;
+      case 'MIT':
+        return `https://opensource.org/licenses/MIT`;
+        break;
+      default:
+        return `http://unlicense.org/`;
+  }
 }
 
+
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  
+function renderLicenseSection(license, fullName) {
+
+  const badge = renderLicenseBadge(license);
+  const link = renderLicenseLink(license);
+
+  switch(license) {
+    case 'Apache':
+      return `
+
+      ${badge} 
+      Copyright 2021 ${fullName}
+
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+
+          ${link}
+
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
+      `;
+      break;
+    case 'Boost':
+      return `        ${badge} ${link}`;
+      break;
+    case 'BSD':
+      return `        ${badge} ${link}`;
+      break;
+    case 'GNU_GPLv2':
+      return `        ${badge} ${link}`;
+      break;
+    case 'GNU_GPLv3':
+      return `        ${badge} ${link}`;
+      break;
+    case 'MIT':
+      return `        ${badge} ${link}`;
+      break;
+    default:
+      return `        ${badge} ${link}`;
+  }
+}
+
+//call with variable filename
+function renderTitleSection(title) {
+  return `#  ${title}
+
+  `;
+}
+
+function renderBody(description, installation, usage) {
+  return `
+  ## Description
+  ${description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contribution Guidelines](#contribution-guidelines)
+  - [Test Instructions](#test-instructions)
+  - [Questions](#questions)
+
+  ## Installation
+  ${installation}
+
+  ## Usage
+  ${usage}
+
+  `;
+}
+
+function renderEndofDoc(contributing, tests, githubName, email) {
+  return `
+  ## Contribution Guidelines 
+  ${contributing}
+
+  ## Test Instructions
+  ${tests}
+
+  ## Questions and Contact Info
+  GitHub Name: ${githubName}
+  GitHub Profile: https://github.com/${githubName}
+  For questions regarding this project, please contact me by email at ${email}. 
+  Thank you for your intrest.
+    `;
 }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  let markdownText = "";
 
-`;
+  const titleSection = renderLicenseSection(license, fullName);
+  const badgeSection = renderLicenseBadge(license);
+  const bodySection = renderBody(description, installation, usage);
+  const licenseSection = renderLicenseSection(license, fullName);
+  const endOfDocSection = renderEndofDoc(contributing, tests, githubName, email);
+
+  markdownText.concat(titleSection, badgeSection, bodySection, licenseSection, endOfDocSection);
+  console.log(markdownText);
 }
 
 module.exports = generateMarkdown;
